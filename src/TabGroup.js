f516ab5cf9967285eb6f295537a2865b917a1a33
Tiger Oakes <tigeroakes@gmail.com>
2016-09-17 14:25:19 -0700

    Fixed tab group CSS so that the margins match that of normal tabs. Updated TabGroup propTypes to ensure that child nodes are all Tab elements

--- src/TabGroup.js
+++ src/TabGroup.js
@@ -1,10 +1,19 @@
-import { PropTypes } from 'react';
+import { PropTypes, Children } from 'react';
+import Tab from './Tab.js';
 
 export default function TabGroup() { return null; }
 
 TabGroup.propTypes = {
 	selected: PropTypes.string,
-	children: PropTypes.node,
 	disabled: PropTypes.bool,
 	title: PropTypes.node,
+	children(props, propName, componentName) {
+		for (const { type } of Children.toArray(props[propName])) {
+			if (type !== Tab) {
+				return new Error(`${componentName} should only have Tab children`);
+			}
+		}
+
+		return null;
+	},
 };
